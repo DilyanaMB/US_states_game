810a1b539fc4fd231c54d68752acb86eda2ad9d8
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-16 00:35:31 +0200

    count guessed states

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ screen.title('U.S. States Game')
 image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
+guessed_states = []
+
+data = pandas.read_csv('50_states.csv')
+states = data.state.tolist()
 
 # def get_mouse_click_coor(x, y):            # Getting coordinates when click upon screen, also keep screen open
 #     print(x, y)
@@ -13,16 +17,17 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 
-answer_state = screen.textinput(title='Guess the State', prompt='What\'s another state\'s name?').title()
-data = pandas.read_csv('50_states.csv')
-states = data.state.tolist()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the State',
+                                    prompt='What\'s another state\'s name?').title()
 
-if answer_state in states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    raw = data[data.state == answer_state]
-    t.goto(raw.x.item(), raw.y.item())
-    t.write(answer_state)
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        raw = data[data.state == answer_state]
+        t.goto(raw.x.item(), raw.y.item())
+        t.write(answer_state)
 
 screen.exitonclick()
